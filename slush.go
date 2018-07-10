0269f6e4d1ad09826967418e1935652d5f3747e9
meherroy <meher.roychowdhury@gmail.com>
2018-07-10 18:44:35 -0400

    Debugged version of Slush, it maybe working

--- slush.go
+++ slush.go
@@ -41,6 +41,7 @@ func (s *slush) networkInit(numnodes int) {
 		s.players[i].identity = i
 		s.players[i].color = uncolored
 		s.players[i].incoming = make(chan message, 100)
+		s.players[i].signal = make(chan bool)
 	}
 }
 
@@ -78,6 +79,7 @@ func (s *slush) handleMsg(index int) {
 
 		// start main slush loop when we first get a color
 		if s.players[index].color != uncolored && !s.players[index].started {
+			s.players[index].started = true
 			go s.slushLoop(index)
 		}
 
@@ -94,7 +96,7 @@ func (s *slush) handleMsg(index int) {
 				s.players[index].countRed++
 				s.players[index].lock.Unlock()
 			}
-
+			fmt.Println(index, "reply", processmsg, s.players[index].countBlue, s.players[index].countRed)
 			// signal slushLoop that we got k  replies
 			if s.players[index].countBlue+s.players[index].countRed == s.k {
 				s.players[index].signal <- true
@@ -113,12 +115,14 @@ func (s *slush) handleMsg(index int) {
 func (s *slush) slushLoop(id int) {
 	// do m rounds of sampling
 	for i := 0; i < s.m; i++ {
+		fmt.Println(id, "slush", i)
 		s.players[id].round = i
 
 		// sample k neigbours
 		sampled := make([]bool, len(s.players))
 		sampled[id] = true
-
+		s.players[id].countRed = 0
+		s.players[id].countBlue = 0
 		for j := 0; j < s.k; j++ {
 			sample := 0
 			// keep trying to get a non-sampled node (SLOW if k ~= len(players))
@@ -129,20 +133,18 @@ func (s *slush) slushLoop(id int) {
 					break // stop retrying
 				}
 			}
-
 			// send query to sample node
 			s.players[sample].incoming <- message{s.players[id].color, id, sample, query, i}
-		}
 
+		}
 		// wait for replies
 		<-s.players[id].signal
-
 		s.players[id].lock.Lock()
 		if float32(s.players[id].countRed) > s.a*float32(s.k) {
 			s.players[id].color = red
 		}
 		if float32(s.players[id].countBlue) > s.a*float32(s.k) {
-			s.players[id].color = blue
+				s.players[id].color = blue
 		}
 		s.players[id].lock.Unlock()
 	}
@@ -165,7 +167,7 @@ func (s *slush) clientinit(num int, color int) {
 	for i := 0; i < num; i++ {
 		index := rand.Intn(len(s.players))
 		s.players[index].incoming <- message{color, -1, index, initialisation, 0}
-
+		fmt.Println(index)
 	}
 }
 
@@ -186,7 +188,7 @@ func main() {
 	sl := slush{
 		a: 0.51,
 		m: 5,
-		k: 5,
+		k: 10,
 	}
 	sl.networkInit(100)
 
@@ -196,5 +198,5 @@ func main() {
 		go sl.handleMsg(i)
 	}
 
-	time.Sleep(1000 * time.Millisecond)
+	time.Sleep(10000 * time.Millisecond)
 }
